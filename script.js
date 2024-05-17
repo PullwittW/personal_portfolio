07800727a175c9b88ec5698b8c4d85ca5395f8b2
Wangu Pullwitt <wangupullwitt.priv@gmail.com>
2024-05-17 13:42:05 +0200

    fixed burger menu

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function toggleMenu() {
+ function toggleMenu() {
     const menu = document.querySelector(".menu-links")
     const icon = document.querySelector(".hamburger-icon")
     menu.classList.toggle("open")
     icon.classList.toggle("open")
+}
+
+function toggleMenuOpen() {
+    document.getElementById("hamburger-nav").style.display = "block";
+}
+
+function toggleMenuClose() {
+    document.getElementById("hamburger-nav").style.display = "none";
 }
