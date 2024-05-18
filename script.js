0f7880b15a52c3ab68023c40c153f819e594db4b
Wangu Pullwitt <wangupullwitt.priv@gmail.com>
2024-05-18 12:32:41 +0200

    changes on photo grid (not finished)

--- script.js
+++ script.js
@@ -4,3 +4,12 @@
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function toggleUSA() {
+    var usa = document.getElementById("usa");
+    if (usa.style.display === "none") {
+        usa.style.display = "grid";
+    } else {
+        usa.style.display = "none";
+    } 
+}
