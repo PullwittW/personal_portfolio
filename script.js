05bc34b95b8ad50b004e9d3630ad16f0cd64ee8e
Wangu Pullwitt <wangupullwitt.priv@gmail.com>
2024-07-07 21:15:05 +0200

    re-built mobile website - deleted explorations - renewed links - resized pictures

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
- function toggleMenu() {
+function toggleMenu() {
     const menu = document.querySelector(".menu-links")
     const icon = document.querySelector(".hamburger-icon")
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
 
-function toggleUSA() {
-    var usa = document.getElementById("usa");
-    if (usa.style.display === "none") {
-        usa.style.display = "grid";
-    } else {
-        usa.style.display = "none";
-    } 
-}
-
-// photo gallery mobile 
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-function plusSlidesUsaCanyons(n) {
-  showSlides(slideIndex += n);
-}
-function plusSlidesUsaLasVegas(n) {
   showSlides(slideIndex += n);
 }
 
@@ -53,16 +36,47 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
+let slideIndexBar = 1;
+showSlides(slideIndexBar);
+
+// Next/previous controls
+function plusSlidesBar(n) {
+  showSlides(slideIndexBar += n);
+}
+
+// Thumbnail image controls
+function currentSlideBar(n) {
+  showSlidesBar(slideIndexBar = n);
+}
+
+function showSlidesBar(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlidesBar");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndexBar = 1}
+  if (n < 1) {slideIndexBar = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
 function openTabDesctop(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("gallery");
-  for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+  if (window.innerWidth >= 615) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("gallery");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinkDesc");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "grid";
+    evt.currentTarget.className += " active";
   }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "grid";
-  evt.currentTarget.className += " active";
 }
