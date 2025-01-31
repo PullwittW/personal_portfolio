43595580333e8b9dfdb772ebb538f196b0f01eac
Wangu Pullwitt <info.pullwitt@gmail.com>
2025-01-31 15:45:23 +0800

    ui improvement

--- script.js
+++ script.js
@@ -5,25 +5,6 @@ function toggleMenu() {
     icon.classList.toggle("open")
 }
 
-// function openTabDesctop(evt, tabName) {
-//   if (window.innerWidth >= 615) {
-//     var i, tabcontent, tablinks;
-//     tabcontent = document.getElementsByClassName("gallery");
-//     for (i = 0; i < tabcontent.length; i++) {
-//       tabcontent[i].style.display = "none";
-//     }
-//     tablinks = document.getElementsByClassName("tablink");
-//     for (i = 0; i < tablinks.length; i++) {
-//         tablinks[i].className = tablinks[i].className.replace(" active", "");
-//         tablinks[i].style.textDecoration = "none"
-//     }
-//     document.getElementById(tabName).style.display = "grid";
-
-//     evt.currentTarget.className += " active";
-//     evt.currentTarget.style.textDecoration = "underline"
-//   }
-// }
-
 // Carousel explorations
 const track = document.querySelector('.carousel-track');
 const items = Array.from(track.children);
@@ -54,39 +35,6 @@ nextButton.addEventListener('click', () => {
 });
 
 updateCarousel();
-
-// Open images in full screen
-document.addEventListener('DOMContentLoaded', function() {
-  const galleries = document.querySelectorAll('.gallery');
-  const overlay = document.getElementById('fullscreen-overlay');
-  const fullscreenImage = document.getElementById('fullscreen-image');
-  const closeBtn = document.querySelector('.close-btn');
-
-  galleries.forEach(gallery => {
-    gallery.addEventListener('click', function(e) {
-      if (e.target.classList.contains('experience-photos')) {
-        overlay.style.display = 'block';
-        fullscreenImage.src = e.target.src;
-        fullscreenImage.alt = e.target.alt;
-      }
-    });
-  });
-
-  closeBtn.addEventListener('click', function() {
-    overlay.style.display = 'none';
-  });
-
-  overlay.addEventListener('click', function(e) {
-    if (e.target === overlay) {
-      overlay.style.display = 'none';
-    }
-  });
-});
-
-// Automatisches Resizing der Koordinaten der Image-Map
-document.addEventListener("DOMContentLoaded", function() {
-  imageMapResize();
-});
 
 
 // Code für Popup auf explorations
